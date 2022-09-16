a684f3ce45425d066f36e64845a3d4f0ef653900
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-09-16 22:48:57 +0200

    doesnt work

--- examples/gcp-kms-gcs-hello/__main__.py
+++ examples/gcp-kms-gcs-hello/__main__.py
@@ -9,6 +9,11 @@ gcp_kms_minimal_bucket = storage.Bucket('my-bucket', location="US")
 pulumi.export('bucket_name', gcp_kms_minimal_bucket.url)
 
 print("TODO(ricc): also print/output a password")
+config = pulumi.Config()
+
 
 # pulumi.export('clear-password',  pulumi.Config().require_secret('clear-password'))
 # pulumi.export('secret-password', pulumi.Config().require_secret('secret-password'))
+print("Lets observe the output behaviour of pulumi:")
+for pwd_key in ["'clear-password', 'secret-password']:
+    print('Password ({}): {}'.format(pwd_key, config.require(pwd_key)))
