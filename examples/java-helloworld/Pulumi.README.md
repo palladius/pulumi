122394650d79d89c0a1f1caea4fd5d7d46246972
Riccardo Carlesso <palladiusbonton@gmail.com>
2022-10-05 21:24:09 +0200

    it works finally

---
+++ examples/java-helloworld/Pulumi.README.md
@@ -0,0 +1,11 @@
+This is my very firdt project in Java.
+I dont like Java but this seems the only way to Ruby via JRuby so I'll suck it up :)
+
+Test:
+
+* Bucket: ${output.my-bucket}
+
+also
+
+1. Reference a string stack output: ${outputs.strVar}
+2. Reference an array stack output: ${outputs.arrVar[1]}
--- examples/java-helloworld/src/main/java/myproject/App.java
+++ examples/java-helloworld/src/main/java/myproject/App.java
@@ -4,15 +4,34 @@ import com.pulumi.Pulumi;
 import com.pulumi.core.Output;
 import com.pulumi.gcp.storage.Bucket;
 import com.pulumi.gcp.storage.BucketArgs;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 
 public class App {
+
     public static void main(String[] args) {
         Pulumi.run(ctx -> {
             var bucket = new Bucket("my-bucket",
                                     BucketArgs.builder()
                                     .location("US")
                                     .build());
+            var strVar = "foo";
+            //var arrVar = new String[]{ "fizz", "buzz" };
+
             ctx.export("bucketName", bucket.url());
+
+            try {
+                var readme = Files.readString(Paths.get("./Pulumi.README.md"));
+                ctx.export("strVar", Output.of(strVar));
+                //ctx.export("arrVar", Output.of(arrVar));
+                ctx.export("readme", Output.of(readme));
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
         });
+
     }
 }
